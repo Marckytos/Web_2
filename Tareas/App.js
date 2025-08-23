f21ca6b7f0263e7b3010e800a18f673d5df5f50b
Marckytos <marquillos.bros3451@gmail.com>
2025-08-23 13:11:37 -0600

    Tarea sobre mi

---
+++ Tareas/App.js
@@ -0,0 +1,21 @@
+function edad() {
+    alert("Tengo 22 años");
+}
+
+function peso() {
+    alert("Peso 58 kg");
+}
+
+function estatura() {
+    alert("Mido 1.64 m");
+}
+
+function deboMaterias() {
+    let debo = false;
+    if (debo) {
+        alert("Debo materias");
+    } else {
+        alert("No debo materias");
+    }
+}
+
