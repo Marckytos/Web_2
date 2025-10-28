ad1c6b65f40eed58a4f3c4e595878d45324062f6
Marckytos <marquillos.bros3451@gmail.com>
2025-10-28 10:52:50 -0600

    Introduccion a node y express

---
+++ clase_28102025/EXPRESS/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get("/",(req,res)=>{
+    res.send("Hola mundo");
+});
+
+const port = 3025;
+
+app.listen(port,()=>{
+    console.log(`Server: http://localhost:${port}`)
+});
